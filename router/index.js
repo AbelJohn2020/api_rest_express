96eb164e4e0380556ec452f0beaa4c612cb8dff5
Abel AR <abel_john_14@hotmail.com>
2021-09-07 16:26:14 -0500

    Add user id in the api

--- router/index.js
+++ router/index.js
@@ -10,5 +10,21 @@ module.exports = function(app) {
         }
     });
 
-    
+    app.get('/users/:id', async ( req, res ) => {
+        try {
+            const userId = req.params.id;
+            const users = await Users.find()
+            const user = users.find( user => user.user_id === userId );
+            if(user) {
+                res.json(user);
+            } else {
+                res.json({
+                    status: 404,
+                    message: `User with ID ${userId} not found`
+                });
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    })
 };
